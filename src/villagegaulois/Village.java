deec8980d7e3b002e745fe68595838923d519d26
KasiaxX <kasialasicav@gmail.com>
2025-09-25 09:47:03 +0200

    fin tp1

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -8,6 +8,7 @@ public class Village {
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
+	private Marche marche; // les attributs sont pour stocker
 
 	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
@@ -45,15 +46,72 @@ public class Village {
 	public String afficherVillageois() {
 		StringBuilder chaine = new StringBuilder();
 		if (nbVillageois < 1) {
-			chaine.append("Il n'y a encore aucun habitant au village du chef "
-					+ chef.getNom() + ".\n");
+			chaine.append("Il n'y a encore aucun habitant au village du chef " + chef.getNom() + ".\n");
 		} else {
-			chaine.append("Au village du chef " + chef.getNom()
-					+ " vivent les légendaires gaulois :\n");
+			chaine.append("Au village du chef " + chef.getNom() + " vivent les légendaires gaulois :\n");
 			for (int i = 0; i < nbVillageois; i++) {
 				chaine.append("- " + villageois[i].getNom() + "\n");
 			}
 		}
 		return chaine.toString();
 	}
+
+	// class interne
+	private class Marche {
+
+		private Etal[] etals; // l'attribut (tableau) de type Etal;
+
+		private Marche(int NbEtals) {
+			etals = new Etal[NbEtals];
+		}
+
+		private void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
+			etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit);
+		}
+
+		private int trouverEtalLibre() {
+			for (int i = 0; i < etals.length; i++)
+				if (etals[i].isEtalOccupe() == false) {
+					return i;
+				}
+			return -1;
+		}
+
+		private Etal[] trouverEtals(String produit) {
+
+			int NbOccupe = 0;
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].contientProduit(produit)) {
+					NbOccupe++;
+				}
+			}
+
+			Etal[] tab = new Etal[NbOccupe];
+			int j = 0;
+
+			for (int i = 0; i < NbOccupe; i++) {
+				if (etals[i].contientProduit(produit)) {
+					tab[j] = etals[i];
+					j++;
+				}
+			}
+			return tab;
+		}
+		
+		private Etal trouverVendeur(Gaulois gaulois) {
+			
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].getVendeur() == gaulois) {
+					return etals[i];
+				}
+			}
+			
+			return null;
+		}
+		
+		private void afficherMarche() {
+			
+		}
+	}
+
 }
